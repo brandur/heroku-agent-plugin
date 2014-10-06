8cfda312433304fca228272bd730953f767afce0
Brandur <brandur@mutelight.org>
2014-10-06 07:33:11 -0700

    Use new style of Unix socket + HTTP proxy
    See https://github.com/excon/excon/pull/427 for more information.

--- init.rb
+++ init.rb
@@ -1,5 +1,4 @@
 # Don't activate if the client is already specifying a proxy.
 if ENV["HEROKU_AGENT_SOCK"] && !ENV["HTTP_PROXY"]
-  ENV["HTTP_PROXY"] = "unix:///"
-  ENV["HTTP_PROXY_SOCKET"] =  File.expand_path(ENV["HEROKU_AGENT_SOCK"])
+  ENV["HTTP_PROXY"] = "unix://#{File.expand_path(ENV["HEROKU_AGENT_SOCK"])}"
 end
